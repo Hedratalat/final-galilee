4f7918c6ede34272e7881821db148de733d36794
hedra talat <hedratalat1717@gmail.com>
2025-12-28 17:12:05 +0200

    admin

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -2,12 +2,13 @@ import { Navigate } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../firebase";
 
+const allowedEmails = ["hedratalat999@gmail.com", "minabahir353@gmail.com"];
 export default function ProtectedRoute({ children }) {
   const [user, loading] = useAuthState(auth);
 
   if (loading) return <p>Loading...</p>;
 
-  if (!user || user.email !== "hedratalat1717@gmail.com") {
+  if (!user || !allowedEmails.includes(user.email)) {
     return <Navigate to="/" replace />;
   }
 
