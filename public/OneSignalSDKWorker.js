f887741a875188b861abc026c4744fc6cd2a7e21
hedra talat <hedratalat1717@gmail.com>
2026-04-25 13:29:52 +0300

    update notif

--- public/OneSignalSDKWorker.js
+++ public/OneSignalSDKWorker.js
@@ -1,1 +1,7 @@
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
